adf12ceafcfabd3847bdcee9b277f59679a14bb3
Mike Bradshaw <mjbradshaw87@gmail.com>
2023-08-30 23:01:01 -0500

    added devices to database init. not a correct solution for others

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { connect } from 'mqtt'
 import { Observable, merge } from 'rxjs'
+import { tap } from 'rxjs/operators'
 import { createServer, httpListener } from '@marblejs/http'
 import { logger$ } from '@marblejs/middleware-logger'
 import { bodyParser$ } from '@marblejs/middleware-body'
@@ -18,31 +19,48 @@ import { laundryMotionDetectorEvent } from 'entities/laundry'
 /**************
  * ZIBGEE
  * ***********/
-const subscriptions$: any = new Observable(observer => {
-  const client: MqttClient = connect('mqtt://localhost:1883')
-  try {
-    client.on('connect', () => {
-      console.log('Connected to MQTT broker')
-      client.subscribe(['z2m/property/#', 'z2m/home/#'], error => {
-        console.log('Reading Subscription')
-        if (error !== null) {
-          observer.error(error)
-        } else {
-          client.on('message', (topic, buffer) => {
-            const [_, __, entity, device] = topic.split('/')
-            observer.next({
-              topic,
-              entity,
-              device,
-              payload: JSON.parse(buffer.toString())
-            })
-          })
-        }
+const bridge$: any = new Observable(observer => {
+  const client = connect('mqtt://localhost:1883')
+  client.on('connect', () => {
+    client.subscribe('z2m/bridge/event', err => {
+      if (err !== null) {
+        console.error(`devices subscription error ${err.message}`)
+        observer.error(err)
+      }
+
+      client.on('message', (topic, buffer) => {
+        console.log(`topic: ${topic} payload: ${buffer.toString()}`)
+        observer.next({topic, payload: JSON.parse(buffer.toString())})
       })
     })
-  } catch (error) {
-    observer.error(error)
+  })
+
+  return () => {
+    if (!client.disconnected) {
+      client.end()
+    }
   }
+})
+
+const subscriptions$: any = new Observable(observer => {
+  const client: MqttClient = connect('mqtt://localhost:1883')
+  client.on('connect', () => {
+    client.subscribe(['z2m/property/#', 'z2m/home/#'], error => {
+      if (error !== null) {
+        observer.error(error)
+      }
+
+      client.on('message', (topic, buffer) => {
+        const [_, __, entity, device] = topic.split('/')
+        observer.next({
+          topic,
+          entity,
+          device,
+          payload: JSON.parse(buffer.toString())
+        })
+      })
+    })
+  })
 
   return () => {
     if (!client.disconnected) {
--- src/database/init.ts
+++ src/database/init.ts
@@ -1,8 +1,9 @@
 import { Database } from 'sqlite3'
-import { connect } from 'mqtt'
+import fs from 'fs'
 
-import type { BridgeMessageDevicesEvent } from 'data/events'
 import config from 'configuration.json'
+
+const logError = (err: Error) => console.error(`ERROR LOGGING DEVICE: ${err.message}\n${err.stack}`)
 
 /**
  * Drops all tables and creates new ones
@@ -35,31 +36,83 @@ const initDatabase = (): void => {
 
     console.log('CREATING TABLE "action_audit"')
     db.run('CREATE TABLE action_audit (id INTEGER, date_time TEXT, uuid TEXT, topic TEXT, payload TEXT, PRIMARY KEY(id))')
-
-    console.log('ALL TABLES CREATED, FINDING ANY ALREADY CONNECTED DEVICES...')
-  })
-  const client = connect('mqtt://localhost:1883')
-  client.subscribe('z2m/bridge/devices', (err) => {
-    if (err !== null) {
-      console.error(err)
-    }
-
-    client.on('message', (topic, buffer) => {
-      const message: BridgeMessageDevicesEvent[] = JSON.parse(buffer.toString())
-      db.serialize(() => {
-        for (const device of message) {
-          console.log(`INSERTING DEVICE ${device.ieee_address}`)
-          db.run(
-            'INSERT INTO device(ieee_address, friendly_name, description) VALUES (?, ?, ?)',
-            [device.ieee_address, device.friendly_name, device.description],
-            (err) => console.error(err)
-          )
-        }
-      })
-    })
-    client.end()
   })
 
+  db.serialize(() => {
+    console.log('ADDING DEVICES...')
+    db.run(
+      'INSERT INTO device(friendly_name, display_name, location, type, description, ieee_address) VALUES(?, ?, ?, ?, ?, ?)',
+      ['property/bed_4/soil_mositure/sensor_1', 'Bed 4 Soil Moisture', 'bed 4', 'soil_moisture', 'Soil moisture sensor for bed 4', '0xa4c12866ec52b2ac'],
+      (err) => err !== null ? logError(err) : ''
+    )
+
+    db.run(
+      'INSERT INTO device(friendly_name, display_name, location, type, description, ieee_address) VALUES(?, ?, ?, ?, ?, ?)',
+      ['property/chicken_coop/temp_humidity/sensor_1', 'Chicken coop temp and humidity', 'chicken coop', 'temp_and_humidity', 'Temperature and humidty in chicken coop', '0x1c34f1fffeecdd6f'],
+      (err) => err !== null ? logError(err) : ''
+    )
+
+    db.run(
+      'INSERT INTO device(friendly_name, display_name, location, type, description, ieee_address) VALUES(?, ?, ?, ?, ?, ?)',
+      ['property/bed_5/soil_mositure/sensor_1', 'Bed 5 Soil Moisture', 'bed 5', 'soil_moisture', 'Soil moisture sensor for bed 5', '0x41c1381d2125ad62'],
+      (err) => err !== null ? logError(err) : ''
+    )
+
+    db.run(
+      'INSERT INTO device(friendly_name, display_name, location, type, description, ieee_address) VALUES(?, ?, ?, ?, ?, ?)',
+      ['property/bed_2/soil_mositure/sensor_1', 'Bed 2 Soil Moisture', 'bed_2', 'soil_moisture', 'Soil moisture sensor for bed 2', '0xa4c13829c00d5078'],
+      (err) => err !== null ? logError(err) : ''
+    )
+
+    db.run(
+      'INSERT INTO device(friendly_name, display_name, location, type, description, ieee_address) VALUES(?, ?, ?, ?, ?, ?)',
+      ['home/lola/light/light_1', 'Lola Overhead Light', 'lola', 'light', 'Light 1 for Lolas room', '0xb0ce18140015ba9a'],
+      (err) => err !== null ? logError(err) : ''
+    )
+
+    db.run(
+      'INSERT INTO device(friendly_name, display_name, location, type, description, ieee_address) VALUES(?, ?, ?, ?, ?, ?)',
+      ['home/master_bath/light/light_1', 'Master Bathroom Light', 'master_bath', 'light', 'Light 1 for master bathroom', '0xb0ce18140015b105'],
+      (err) => err !== null ? logError(err) : ''
+    )
+
+    db.run(
+      'INSERT INTO device(friendly_name, display_name, location, type, description, ieee_address) VALUES(?, ?, ?, ?, ?, ?)',
+      ['home/master_bath/motion_sensor/bathroom', 'Master Bathroom Motion Sensor', 'master_bath', 'motion_sensor', 'Motion sensor in master bathroom', '0x282c02bfffed3441'],
+      (err) => err !== null ? logError(err) : ''
+    )
+
+    db.run(
+      'INSERT INTO device(friendly_name, display_name, location, type, description, ieee_address) VALUES(?, ?, ?, ?, ?, ?)',
+      ['home/master_bath/light/light_2', 'Master Bathroom Light', 'master_bath', 'light', 'Light 2 for master bathroom', '0xb0ce18140015be41'],
+      (err) => err !== null ? logError(err) : ''
+    )
+
+    db.run(
+      'INSERT INTO device(friendly_name, display_name, location, type, description, ieee_address) VALUES(?, ?, ?, ?, ?, ?)',
+      ['home/lola/light/light_2', 'Lola Overhead Light', 'lola', 'light', 'Light 2 for Lolas room', '0xb0ce18140015ae57'],
+      (err) => err !== null ? logError(err) : ''
+    )
+
+    db.run(
+      'INSERT INTO device(friendly_name, display_name, location, type, description, ieee_address) VALUES(?, ?, ?, ?, ?, ?)',
+      ['home/laundry/motion_sensor/laundry', 'Laundry Motion Sensor', 'laundry', 'motion_sensor', 'Motion sensor in laundry room', '0x282c02bfffed39e8'],
+      (err) => err !== null ? logError(err) : ''
+    )
+
+    db.run(
+      'INSERT INTO device(friendly_name, display_name, location, type, description, ieee_address) VALUES(?, ?, ?, ?, ?, ?)',
+      ['home/laundry/light/light_1', 'Laundry Light', 'laundry', 'light', 'Light 1 in laundry room', '0xb0ce1814036753ea'],
+      (err) => err !== null ? logError(err) : ''
+    )
+
+    db.run(
+      'INSERT INTO device(friendly_name, display_name, location, type, description, ieee_address) VALUES(?, ?, ?, ?, ?, ?)',
+      ['home/laundry/light/light_2', 'Laundry Light', 'laundry', 'light', 'Light 2 in laundry room', '0xb0ce18140367646d'],
+      (err) => err !== null ? logError(err) : ''
+    )
+  })
+  console.log('DONE')
   db.close()
 }
 
