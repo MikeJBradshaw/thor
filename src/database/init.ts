e1ebdeba3481ce48ee2a7d85bcb32927b55753ad
Mike Bradshaw <mjbradshaw87@gmail.com>
2023-10-06 15:01:26 -0500

    database init is sketchy, dont code when tired

--- src/database/init.ts
+++ src/database/init.ts
@@ -45,7 +45,7 @@ const initDatabase = (z2mConfig: string): void => {
     db.run('CREATE TABLE entity_device (id INTEGER, entity_id INTEGER, device_id INTEGER, FOREIGN KEY (entity_id) REFERENCES entity(id), FOREIGN KEY (device_id) REFERENCES device(id), PRIMARY KEY(id))')
 
     console.log('CREATING TABLE "transition"')
-    db.run('CREATE TABLE state (id: INTEGER, device_id INTEGER, start_time INTEGER, duration INTEGER, end_time INTEGER), PRIMARY KEY(id), FOREIGN KEY (device_id) REFERENCES device(id)')
+    db.run('CREATE TABLE state (id INTEGER, device_id INTEGER, start_time INTEGER, duration INTEGER, end_time INTEGER, PRIMARY KEY(id), FOREIGN KEY (device_id) REFERENCES device(id))')
   })
 
   db.serialize(() => {
@@ -53,43 +53,44 @@ const initDatabase = (z2mConfig: string): void => {
     for (const entity of ENTITIES) {
       console.log(' entity:', entity)
       const name = entity.replace('_', ' ').toUpperCase()
+      console.log(' name:', name)
       db.run('INSERT INTO entity(name, key) VALUES (?, ?)', [name, entity], err => err !== null ? logError(err) : () => {})
     }
   })
 
-  console.log('GETTING DEVICE INFO FROM CONFIG FILE...')
-  const configDoc = load(readFileSync(z2mConfig, 'utf-8')) as Z2mConfigYaml
-  const devices = configDoc.devices
+  // console.log('GETTING DEVICE INFO FROM CONFIG FILE...')
+  // const configDoc = load(readFileSync(z2mConfig, 'utf-8')) as Z2mConfigYaml
+  // const devices = configDoc.devices
 
-  console.log('ADDING DEVICES...')
-  db.serialize(() => {
-    for (const [ieeeAddress, deviceInfo] of Object.entries(devices)) {
-      const [_, entity, type, __] = deviceInfo.friendly_name.split('/')
-      if (entity === undefined || type === undefined) {
-        continue
-      }
+  // console.log('ADDING DEVICES...')
+  // db.serialize(() => {
+  //   for (const [ieeeAddress, deviceInfo] of Object.entries(devices)) {
+  //     const [_, entity, type, __] = deviceInfo.friendly_name.split('/')
+  //     if (entity === undefined || type === undefined) {
+  //       continue
+  //     }
 
-      console.log(' device:', deviceInfo.friendly_name)
-      db.run(
-        'INSERT INTO device(friendly_name, display_name, description, ieee_address) VALUES (?, ?, ?, ?)',
-        [deviceInfo.friendly_name, deviceInfo.display_name, deviceInfo.description, ieeeAddress],
-        function (err: Error) {
-          if (err !== null) {
-            logError(err)
-            return
-          }
-          return this.lastID
-        }
-      )
+  //     console.log(' device:', deviceInfo.friendly_name)
+  //     db.run(
+  //       'INSERT INTO device(friendly_name, display_name, description, ieee_address) VALUES (?, ?, ?, ?)',
+  //       [deviceInfo.friendly_name, deviceInfo.display_name, deviceInfo.description, ieeeAddress],
+  //       function (err: Error) {
+  //         if (err !== null) {
+  //           logError(err)
+  //           return
+  //         }
+  //         return this.lastID
+  //       }
+  //     )
 
-      const entityIndex = ENTITIES.indexOf(entity) + 1
-      db.run(
-        'INSERT INTO entity_device(device_id, entity_id) VALUES(last_insert_rowid(), ?)',
-        [entityIndex],
-        (err: Error) => err !== null ? logError(err) : () => { }
-      )
-    }
-  })
+  //     const entityIndex = ENTITIES.indexOf(entity) + 1
+  //     db.run(
+  //       'INSERT INTO entity_device(device_id, entity_id) VALUES(last_insert_rowid(), ?)',
+  //       [entityIndex],
+  //       (err: Error) => err !== null ? logError(err) : () => { }
+  //     )
+  //   }
+  // })
 
   console.log('DONE')
   db.close()
@@ -102,26 +103,26 @@ if (LEVEL !== PRODUCTION && LEVEL !== TEST) {
   process.exit(-1)
 }
 
-if (PRIVATE_KEY_PATH === undefined || PRIVATE_KEY_PATH === '') {
+if (LEVEL === TEST && (PRIVATE_KEY_PATH === undefined || PRIVATE_KEY_PATH === '')) {
   console.error('Private key path not found. Set "private_key_path" and retry.')
   process.exit(-1)
 }
 
 (async () => {
-  try {
-    if (LEVEL === 'test') {
-      const scpClient = await Client({
-        host: '10.243.31.95',
-        port: 22,
-        username: 'mike',
-        privateKey: readFileSync(PRIVATE_KEY_PATH)
-      })
-      await scpClient.downloadFile(config.z2mConfigLocation, TMP_CONFIG_FILE)
-      scpClient.close()
-    }
-  } catch (err) {
-    console.error(err)
-  }
+  // try {
+  //   if (LEVEL == TEST) {
+  //     const scpClient = await Client({
+  //       host: '10.243.31.95',
+  //       port: 22,
+  //       username: 'mike',
+  //       privateKey: readFileSync(PRIVATE_KEY_PATH)
+  //     })
+  //     await scpClient.downloadFile(config.z2mConfigLocation, TMP_CONFIG_FILE)
+  //     scpClient.close()
+  //   }
+  // } catch (err) {
+  //   console.error(err)
+  // }
 
   initDatabase(LEVEL === PRODUCTION ? config.z2mConfigLocation : TMP_CONFIG_FILE)
 })().catch(e => console.error(e))
